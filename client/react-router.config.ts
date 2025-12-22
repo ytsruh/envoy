292f0831c5722656d7bb1a30443917af3012bfe6
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2025-12-22 20:31:26 +0000

    chore: removed unused react client

--- client/react-router.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
-  async prerender() {
-    return ["/"];
-  },
-} satisfies Config;
